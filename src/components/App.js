93be19e36113567b081a05df8955e3b47fe29063
MHammadYou <MHammadY2.0@gmail.com>
2021-07-09 14:19:00 +0500

    Boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>React-Electron-Boilerplate</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/electron/index.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const window = new BrowserWindow();
+
+  window.loadFile("build/index.html");
+};
+
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => app.quit());
+
+try {
+  require("electron-reloader")(module);
+} catch (_) {}
